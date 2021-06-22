d5760f0871a7fa5cc85d2d462862d43f7b7cab5a
broccolino <81223327+broccolino1v9@users.noreply.github.com>
2021-06-22 13:44:17 +0200

    Add files via upload

---
+++ Sketchbook/src/model/dao/ProductDAO.java
@@ -0,0 +1,221 @@
+package model.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Collection;
+import java.util.LinkedList;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+import java.util.*;
+import model.bean.ProductBean;
+import model.bean.UserBean;
+
+public class ProductDAO {
+	private static DataSource ds;
+	public static final String TABLE_NAME1 = "prodotto";
+	public static final String TABLE_NAME2 = "dettagli_ordine";
+	static {
+		try {
+			Context initCtx = new InitialContext();
+			Context envCtx = (Context) initCtx.lookup("java:comp/env");
+
+			ds = (DataSource) envCtx.lookup("jdbc/ecommerce");
+
+		} catch (NamingException e) {
+			System.out.println("Error:" + e.getMessage());
+		  }
+	}
+	
+	public synchronized Collection<ProductBean> doRetrieveAll(String order) throws SQLException{
+		
+		Connection connection = null;
+		PreparedStatement preparedStatement = null;
+
+		Collection<ProductBean> prodotti = new LinkedList<ProductBean>();
+
+		String selectSQL = "SELECT * FROM " + ProductDAO.TABLE_NAME1;
+
+		if (order != null && !order.equals("")) {
+			selectSQL += " ORDER BY " + order;
+		}
+
+		try {
+			connection = ds.getConnection();
+			preparedStatement = connection.prepareStatement(selectSQL);
+
+			ResultSet rs = preparedStatement.executeQuery();
+
+			while (rs.next()) {
+				ProductBean bean = new ProductBean();
+				
+				bean.setId(rs.getInt("id"));
+				bean.setNome(rs.getString("nome"));
+				bean.setDescrizione(rs.getString("descrizione"));
+				bean.setPrezzo(rs.getDouble("prezzo"));
+				bean.setIva(rs.getDouble("iva"));
+				bean.setQuantitaCatalogo(rs.getInt("Quantità"));
+				
+				prodotti.add(bean);
+			}
+
+		} finally {
+			try {
+				if (preparedStatement != null)
+					preparedStatement.close();
+			} finally {
+				if (connection != null)
+					connection.close();
+			}
+		}
+		return prodotti;
+	}
+	
+	
+	public synchronized ProductBean doRetrieveByKey(int code) throws SQLException {
+		Connection connection = null;
+		PreparedStatement preparedStatement = null;
+
+		ProductBean bean = new ProductBean();
+
+		String selectSQL = "SELECT * FROM " + ProductDAO.TABLE_NAME1 + " WHERE CODE = ?";
+
+		try {
+			connection = ds.getConnection();
+			preparedStatement = connection.prepareStatement(selectSQL);
+			preparedStatement.setInt(1, code);
+
+			ResultSet rs = preparedStatement.executeQuery();
+
+			while (rs.next()) {
+				bean.setId(rs.getInt("id"));
+				bean.setNome(rs.getString("nome"));
+				bean.setDescrizione(rs.getString("descrizione"));
+				bean.setPrezzo(rs.getDouble("prezzo"));
+				bean.setIva(rs.getDouble("iva"));
+				bean.setQuantitaCatalogo(rs.getInt("Quantità"));
+			}
+
+		} finally {
+			try {
+				if (preparedStatement != null)
+					preparedStatement.close();
+			} finally {
+				if (connection != null)
+					connection.close();
+			}
+		}
+		return bean;
+	}
+	
+	public synchronized List<ProductBean> doRetrieveByOrder(int code) throws SQLException {
+		{
+			Connection connection = null;
+			PreparedStatement preparedStatement = null;
+			
+			List<ProductBean> prodotti = new ArrayList<ProductBean>();
+			ProductBean bean = new ProductBean();
+
+			String selectSQL = "SELECT ID_PRODOTTO, NOMEPRODOTTO, PREZZOPRODOTTO, IVAPRODOTTO" +
+								"DESCRIZIONEPRODOTTO, QUANTITà " +
+								"FROM " + ProductDAO.TABLE_NAME2 + "JOIN" + OrderDAO.TABLE_NAME1 +
+								"ON ID_ORDINE = ID" +
+								"WHERE CODE = ?";
+
+			try {
+				connection = ds.getConnection();
+				preparedStatement = connection.prepareStatement(selectSQL);
+				preparedStatement.setInt(1, code);
+
+				ResultSet rs = preparedStatement.executeQuery();
+
+				while (rs.next()) {
+					bean.setId(rs.getInt("ID_PRODOTTO"));
+					bean.setNome(rs.getString("NOMEPRODOTTO"));
+					bean.setDescrizione(rs.getString("DESCRIZIONEPRODOTTO"));
+					bean.setPrezzo(rs.getDouble("PREZZOPRODOTTO"));
+					bean.setIva(rs.getDouble("IVAPRODOTTO"));
+					bean.setQuantitaCarrello(rs.getInt("Quantità"));
+					
+					prodotti.add(bean);
+				}
+
+			} finally {
+				try {
+					if (preparedStatement != null)
+						preparedStatement.close();
+				} finally {
+					if (connection != null)
+						connection.close();
+				}
+			}
+			return prodotti;
+		}
+	}
+	
+	
+	public synchronized boolean doDelete(int code) throws SQLException {
+		Connection connection = null;
+		PreparedStatement preparedStatement = null;
+
+		int result = 0;
+
+		String deleteSQL = "DELETE FROM " + ProductDAO.TABLE_NAME1 + " WHERE CODE = ?";
+
+		try {
+			connection = ds.getConnection();
+			preparedStatement = connection.prepareStatement(deleteSQL);
+			preparedStatement.setInt(1, code);
+
+			result = preparedStatement.executeUpdate();
+
+		} finally {
+			try {
+				if (preparedStatement != null)
+					preparedStatement.close();
+			} finally {
+				if (connection != null)
+					connection.close();
+			}
+		}
+		return (result != 0);
+	}
+	
+	
+	
+
+	public synchronized void doSave(ProductBean product) throws SQLException {
+
+		Connection connection = null;
+		PreparedStatement preparedStatement = null;
+
+		String insertSQL = "INSERT INTO " + ProductDAO.TABLE_NAME1
+				+ "(NOME, DESCRIZIONE, PREZZO, IVA, Quantità) VALUES (?, ?, ?, ?, ?)";
+
+		try {
+			connection = ds.getConnection();
+			preparedStatement = connection.prepareStatement(insertSQL);
+			preparedStatement.setString(1, product.getNome());
+			preparedStatement.setString(2, product.getDescrizione());
+			preparedStatement.setDouble(3, product.getPrezzo());
+			preparedStatement.setDouble(4, product.getIva());
+			preparedStatement.setInt(5, product.getQuantitaCatalogo());
+			
+			preparedStatement.executeUpdate();
+
+			connection.commit();
+		} finally {
+			try {
+				if (preparedStatement != null)
+					preparedStatement.close();
+			} finally {
+				if (connection != null)
+					connection.close();
+			}
+		}
+	}
+}
