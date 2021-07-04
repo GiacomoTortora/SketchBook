bed234d494e6345bf5f3bcdbd9afea6f12d722ec
raffaele <raffa@DESKTOP-MMOUKF6>
2021-07-04 12:26:13 +0200

    checkout jsp

---
+++ Sketchbook/src/control/CheckoutController.java
@@ -0,0 +1,62 @@
+package control;
+
+import java.io.IOException;
+import java.util.Date;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import model.Cart;
+import model.bean.OrderBean;
+import model.bean.ProductBean;
+import model.bean.UserBean;
+import model.dao.OrderDAO;
+
+/**
+ * Servlet implementation class CheckoutController
+ */
+@WebServlet("/CheckoutController")
+public class CheckoutController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+
+    public CheckoutController() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		try {System.out.println("xd");
+				OrderBean ordine = new OrderBean();
+				HttpSession session = request.getSession(false); 
+				
+				Cart cart = (Cart)session.getAttribute("cart");
+				UserBean user = (UserBean) session.getAttribute("currentSessionUser");
+				if(user == null) 
+					user = (UserBean)session.getAttribute("adminSession");
+				
+				List<ProductBean> prodotti = cart.getProducts();
+				
+				ordine.setIdCliente(user.getId());
+				ordine.setData((java.sql.Date) new Date());
+				ordine.setProdotti(prodotti);
+				ordine.setStato("Ordinato");
+				
+				new OrderDAO().doSave(ordine);
+				session.removeAttribute("cart");
+				response.sendRedirect("ProductView.jsp");
+		}catch(Exception e) {
+			
+		}
+	}
+
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
